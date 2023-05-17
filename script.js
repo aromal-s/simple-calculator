06ab0e23606d4cc676ccac59acb43aaa6a4664fa
Aromal S <aromals0479@gmail.com>
2023-05-17 17:47:40 +0530

    function defined in js file & css & html file updated

---
+++ script.js
@@ -0,0 +1,17 @@
+function btnClick(val)
+{
+    document.getElementById("screen").value+=val
+
+}
+
+function clearDisplay()
+{
+    document.getElementById("screen").value=" "
+}
+
+function equalClick()
+{
+    var text=document.getElementById("screen").value
+    var result=eval(text)
+    document.getElementById("screen").value=result
+}
